01e02fd6636193d31e739a41f1a959b1a77df220
yanjigb <yanji.notification@gmail.com>
2024-11-25 13:34:28 +0700

    feat(ui): :sparkles: [#43] Update Explain Formula

--- src/container/dashboards/ads-insight-formula/components/explain/index.tsx
+++ src/container/dashboards/ads-insight-formula/components/explain/index.tsx
@@ -6,13 +6,17 @@ export default function Explain() {
       <CardHeader title="Key Metrics Explained" subheader="Detailed explanation of important ad performance metrics" />
       <CardContent>
         <ul className="list-disc list-inside space-y-2 text-base">
-          <li><strong>CPM (Cost per 1000 Impressions):</strong> Average cost for each 1000 impressions.</li>
+          <li><strong>CPM (Cost per Impression):</strong> Average cost for each impression.</li>
           <li><strong>CPC (Cost per Click):</strong> Average cost for each click on the ad.</li>
           <li><strong>CTR (Click-Through Rate):</strong> Percentage of impressions that resulted in a click.</li>
+          <li><strong>CFI (Total cost from impressions):</strong> Total cost for all impressions.</li>
+          <li><strong>Total Interactions:</strong> Total number of interactions with the ad.</li>
           <li><strong>Engagement Rate:</strong> Percentage of users who interacted with the ad in some way.</li>
+          <li><strong>Discount Percentage (%):</strong> Percentage of the discount applied to the advertise.</li>
+          <li><strong>Discount Amount (VND):</strong> Amount of the discount applied to the advertise.</li>
           <li><strong>Impressions:</strong> Number of times the ad was displayed.</li>
-          <li><strong>Advertise Score:</strong> The higher the more your advertise is efficient and your advertise cost more cheaper.</li>
-          <li><strong>Advertise Cost:</strong> The cost of your advertise after applying the discount.</li>
+          <li><strong>Advertise Score (0 - 100):</strong> The higher the more your advertise is efficient and your advertise cost more cheaper and your ads will be go to more people.</li>
+          <li><strong>Advertise Cost (VND):</strong> The cost of your advertise after applying the discount.</li>
         </ul>
       </CardContent>
     </Card>
--- src/container/dashboards/ads-insight-formula/components/sample/index.tsx
+++ src/container/dashboards/ads-insight-formula/components/sample/index.tsx
@@ -27,34 +27,66 @@ export default function Sample() {
           </TableHead>
           <TableBody>
             <TableRow>
-              <TableCell>CPM (Cost per 1000 Impressions) VND</TableCell>
-              <TableCell>advertise budget / (impressions / 1000)</TableCell>
-              <TableCell>1000000 / (10000 / 1000) = 100000</TableCell>
+              <TableCell>CPM (Cost per Impression) VND</TableCell>
+              <TableCell>advertise budget / impressions</TableCell>
+              <TableCell>160000 / 18 = 8888</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>CFI (Total cost from impressions) VND</TableCell>
+              <TableCell>impressions * CPM</TableCell>
+              <TableCell>18 * 8888 = 159984</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>CPC (Cost per Click) VND</TableCell>
               <TableCell>advertise budget / total clicks</TableCell>
-              <TableCell>1000000 / 150 = 6666.67</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Engagement Rate (%)</TableCell>
-              <TableCell>(total interactions / impressions) * 100</TableCell>
-              <TableCell>(1000 / 10000) * 100 = 10%</TableCell>
+              <TableCell>160000 / 20 = 8000</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>CTR (Click-through rate) %</TableCell>
               <TableCell>(total clicks / impressions) * 100</TableCell>
-              <TableCell>(150 / 10000) * 100 = 1.5%</TableCell>
+              <TableCell>(20 / 18) * 100 = 111.11%</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Total Interactions</TableCell>
+              <TableCell>total items in daily analytics / total clicks in daily analytics</TableCell>
+              <TableCell>20 / 20 = 1</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Engagement Rate %</TableCell>
+              <TableCell>(total interactions / impressions) * 100</TableCell>
+              <TableCell>(1 / 18) * 100 = 5.56%</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Advertise Score (0-100)</TableCell>
               <TableCell>(CTR + Engagement Rate) / 2</TableCell>
-              <TableCell>(1.5% + 10%) / 2 = 5.75</TableCell>
+              <TableCell>(111.11% + 5.56%) / 2 = 58.335</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Total Cost Per Day (VND)</TableCell>
+              <TableCell>total cost from impressions + total cost from clicks</TableCell>
+              <TableCell>159984 + 160000 = 319984</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Discount Percentage (%)</TableCell>
+              <TableCell>( score / 100 ) * 0.3</TableCell>
+              <TableCell>
+                <div>
+                  (58.335 / 100) * 0.3 = 0.175
+                </div>
+                <sub className="text-xs">
+                  (highest discount percentage is 30% so we use 30% if the result is greater than 30%)
+                </sub>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Discount Amount (VND)</TableCell>
+              <TableCell>advertise budget * discount percentage</TableCell>
+              <TableCell>160000 * 0.175 = 28000</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Advertise Cost (VND)</TableCell>
-              <TableCell>advertise budget - discount amount</TableCell>
-              <TableCell>1000000 - 300000 = 700000</TableCell>
+              <TableCell>total cost per day - discount amount</TableCell>
+              <TableCell>319984 - 28000 = 291984</TableCell>
             </TableRow>
           </TableBody>
         </Table>
